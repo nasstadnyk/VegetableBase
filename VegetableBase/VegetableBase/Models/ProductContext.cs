0551f1b568e48e824bb25a9ebd24cefcb6e06b14
nasstadnyk <47760288+nasstadnyk@users.noreply.github.com>
2022-06-08 16:30:20 +0300

    DataAccessLayer

---
+++ VegetableBase/VegetableBase/Models/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace VegetableBase.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("VegetableBase")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        //public DbSet<CartItem> ShoppingCartItem { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
